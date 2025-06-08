bcfde5b7dc3f5536d34d8918e324254a5f122267
Priya <spriyakeziah@gmail.com>
2025-06-08 17:06:23 +0530

    Fix import path for Dashboard component

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Login from './Login'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
 import LoginFailed from './LoginFailed';
 
 function App() {
